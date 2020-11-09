680309b91a7a02ebcd012e3c416d68d459f31e80
seankearon <sean@tackaberry.org>
2020-11-09 08:22:57 +0000

    Added base projects.

---
+++ EntryPointAPI/Controllers/CustomerController.fs
@@ -0,0 +1,14 @@
+﻿namespace EntryPointAPI.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+type CustomerController (logger : ILogger<CustomerController>) =
+    inherit ControllerBase()
+    let _logger = logger
+
+    [<HttpPost>]
+    [<Route("newcustomer")>]
+    member _.Get() =
+        Ok()
