80f503b9254b4db979408997a23e22294c325868
seankearon <sean@tackaberry.org>
2020-11-10 08:22:00 +0000

    Actually added the messages.

---
+++ OnboardingMessages/Messages.fs
@@ -0,0 +1,28 @@
+﻿namespace OnboardingMessages
+
+type CreateCustomerAccount =
+    {
+        Name: string
+        Email: string
+    }
+
+type CustomerAccountCreated =
+    {
+        Email: string
+        CustomerId: int
+    }
+
+type SendWelcomeEmail =
+    {
+        CustomerId: int
+    }
+
+type ScheduleSalesCall =
+    {
+        CustomerId: int
+    }
+
+type CustomerOnboarded =
+    {
+        CustomerId: int
+    }
