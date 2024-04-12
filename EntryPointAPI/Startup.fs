e41cddbaf7f3f418d22c81fe0479572ae2d24e46
seankearon <sean@tackaberry.org>
2024-04-12 13:37:46 +0100

    Updated Rebus to v8.

--- EntryPointAPI/Startup.fs
+++ EntryPointAPI/Startup.fs
@@ -5,30 +5,28 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Rebus.Config
 
 module Lib =
-    open Rebus.Config
     open OnboardingMessages
     open Rebus.Retry.Simple
     open Rebus.Routing.TypeBased
     open Rebus.Transport.FileSystem
 
-    let configure (rebus: RebusConfigurer) (config: IConfiguration) =
+    let configure (config: IConfiguration) (rebus: RebusConfigurer) =
            let asbConnection = config.GetConnectionString("AzureServiceBusConnectionString")
            rebus.Logging   (fun l -> l.Console())                                                  |> ignore
            rebus.Routing   (fun r -> r.TypeBased().Map<OnboardNewCustomer>("MainQueue") |> ignore) |> ignore
            rebus.Transport (fun t -> t.UseFileSystemAsOneWayClient(asbConnection))                 |> ignore
-           rebus.Options   (fun t -> t.SimpleRetryStrategy(errorQueueAddress = "ErrorQueue"))      |> ignore
+           rebus.Options   (fun t -> t.RetryStrategy(errorQueueName = "ErrorQueue"))               |> ignore
            rebus
-
-open Rebus.ServiceProvider
 
 type Startup(configuration: IConfiguration) =
     member _.Configuration = configuration
 
     member this.ConfigureServices(services: IServiceCollection) =
         services.AddControllers()        |> ignore
-        services.AddRebus(fun rebus -> Lib.configure rebus this.Configuration) |> ignore
+        services.AddRebus(Lib.configure this.Configuration) |> ignore
 
     member _.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
         if (env.IsDevelopment()) then
